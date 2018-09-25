510f7705bf6e537860d3f614f77e9cf3b3df2162
damu178 <salemamba@gmail.com>
2018-09-25 07:28:31 -0500

    Adds user authentication token

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,4 +1,4 @@
-require 'spec_helper'
+# require 'spec_helper'
 
 describe User, type: :model do
  	before {@user = FactoryBot.build(:user)}
@@ -13,11 +13,28 @@ describe User, type: :model do
 	# it { should validate_uniqueness_of(:email) }
 	it { should validate_confirmation_of(:password) }
 	it { should allow_value('example@domain.com').for(:email) }
+	it { should respond_to(:auth_token) }
+	it { should validate_uniqueness_of(:auth_token) }
     
     describe "when email is not present" do 
     	before {@user.email = " "}
 	 	it { should_not be_valid } 
 
+	end
+
+
+	describe "#generate_authentication_token!" do 
+		it "generate a unique token" do 
+			Devise.stub(:friendly_token).and_return("auniquetoken123")
+			@user.generate_authentication_token!
+			expect(@user.auth_token).to eql "auniquetoken123"
+		end
+
+		 it "generates another token when one already has been taken" do
+      		existing_user = FactoryBot.create(:user, auth_token: "auniquetoken123")
+      		@user.generate_authentication_token!
+      		expect(@user.auth_token).not_to eql existing_user.auth_token
+    	end
 	end
 
 
