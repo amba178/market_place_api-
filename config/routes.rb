b3e594ac3e1c2270975959b28653a5f0e71f12ca
damu178 <salemamba@gmail.com>
2018-09-25 08:18:19 -0500

    Adds sessions controller create action

--- config/routes.rb
+++ config/routes.rb
@@ -11,7 +11,8 @@ Rails.application.routes.draw do
     scope module: :v1,
               constraints: ApiConstraints.new(version: 1, default: true) do
       # We are going to list our resources here
-       resources :users, :only => [:show, :create, :update, :destroy]
+       resources :users, only: [:show, :create, :update, :destroy]
+       resources :sessions, only:  [:create, :destroy]
     end
   end
 end
