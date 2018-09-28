bc2d1ad06b4b528f20f903543094acd9ea3e4ce1
damu178 <salemamba@gmail.com>
2018-09-28 12:25:27 -0500

    Adds public product endpoints(show, index)

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,4 +3,5 @@ class ApplicationController < ActionController::API
 	  # protect_from_forgery with: :null_session
 	  # before_action :authenticate_user!
 	 include Authenticable 
+	 respond_to :json
 end
---
+++ spec/controllers/api/v1/products_controller_spec.rb
@@ -0,0 +1,34 @@
+require 'spec_helper'
+
+describe Api::V1::ProductsController, type: :controller do
+
+   describe "GET #show" do
+   	 before(:each) do
+      	@product = FactoryBot.create :product
+     	 get :show, params: {id: @product.id}, format: :json 
+     end
+
+    it "returns the information about a reporter on a hash" do
+      product_response = json_response
+      expect(product_response[:title]).to eql @product.title
+    end
+
+       it { should respond_with 200 }
+    end
+
+
+    describe "GET #index" do 
+    	before(:each) do 
+    		4.times { FactoryBot.create :product }
+    		get :index 
+    	end
+
+    	it "returns 4 records from the database" do 
+    		products_response = json_response
+    		# byebug
+    		expect(products_response.size).to eql 4
+    	end
+    	it {should respond_with 200 }
+    end
+
+end
